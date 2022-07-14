72da5bcc6c201187be2df9335d15afbd553e7e73
Mary Nguyen <marylongnguyen@users.noreply.github.com>
2022-07-14 18:21:37 +0000

    proj update

--- alta3research-python-cert/alta3research-pythoncert01.py
+++ alta3research-python-cert/alta3research-pythoncert01.py
@@ -32,29 +32,37 @@ def main():
                         'Cow': 'I am a domestic animal living in the field. I like to eat grass and produce milk. What am I? ', 
                         'Bat': 'I fly all night and sleep at dawn. When I do that I hang upside down. What am I? '}
 
-    riddle = list(riddles_from_csv.values())
-    answer = list(riddles_from_csv.keys())
-
-    question = random.choice(riddle)
-    guess = input(question)
-    
-    riddle_answer_key = riddle.index(question) #list index of riddle to be used to return value from matching index in answer list
-
+    riddles = list(riddles_from_csv.values())
+    answers = list(riddles_from_csv.keys())
+    num_riddles = len(riddles_from_csv)
     guess_count = 1
-  
-    while guess_count < 3 and guess.capitalize() != answer[riddle_answer_key]:
+    riddles_correct = 0
+
+
+    riddle = random.choice(riddles)
+    guess = input(riddle)
+    
+    riddle_answer_key = riddles.index(riddle) #list index of riddle to be used to return value from matching index in answer list
+
+    while guess_count < 3 and guess.capitalize() != answers[riddle_answer_key]:
       guess_count += 1
       guess = input('Nope! Try again. Who am I? ')    
 
-    if guess.capitalize() == answer[riddle_answer_key]:
-        print("Nice! Yes, I'm a(n) " + answer[riddle_answer_key] + " :)")
-    else: 
+    if guess.capitalize() != answers[riddle_answer_key]: 
       print('Sorry, better luck next time!')
 
-    questions_answered = []
-    questions_answered.append(riddle_answer_key)
+    else:
+        riddles_correct += 1
+        vowels = ['A','E','I','O','U'] #used to help generate message for when the answer is correct
 
-    print(questions_answered)
+        if answers[riddle_answer_key][0] in vowels:
+            print("Nice! Yes, I'm an " + answers[riddle_answer_key] + " :)")
+        else:
+            print("Nice! Yes, I'm a " + answers[riddle_answer_key] + " :)")
+
+    riddles_answered = []
+    riddles_answered.append(riddle_answer_key)
+    print(riddles_answered)
 
   # print crayons.red('red string')
 
